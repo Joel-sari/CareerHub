9b5ab7f9945ffd1cc0cf9a62785a6907bffe9d2a
Amrin Khimani <amrinkhimani@Amrins-MacBook-Air.local>
2025-09-24 20:29:23 -0400

    Resolved merge conflicts from base-html-fix

--- CareerHub/home/views.py
+++ CareerHub/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+
+def login_view(request):
+    return HttpResponse("Login page placeholder")
+
+def register_view(request):
+    return HttpResponse("Register page placeholder")
