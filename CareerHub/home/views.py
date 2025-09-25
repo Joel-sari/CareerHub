d4483256f01fa5089d9b60f99c72caff75161944
Amrin Khimani <amrinkhimani@Amrins-MacBook-Air.local>
2025-09-24 20:58:13 -0400

    Created a home.css file

--- CareerHub/home/views.py
+++ CareerHub/home/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+# Home page
 def home(request):
     return render(request, 'home/home.html')
 
+# Login placeholder
 def login_view(request):
     return HttpResponse("Login page placeholder")
 
+# Register placeholder
 def register_view(request):
     return HttpResponse("Register page placeholder")
