1d6a59bc34f4ed21b0317ceba7c4137b3e65072d
Jacquelyn Phan <jacquelynphan108@gmail.com>
2025-10-22 05:31:13 -0400

    Created reusable UI components, and stylized home, login, sign up, and edit profile for both recruiters and jobseekers.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./**/templates/**/*.html",
+    "./static/js/**/*.js",
+    "./static/css/**/*.css"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#42547C",
+        accent: "#EFCE63",
+        success: "#26B509",
+        warning: "#F36E6E",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        arimo: ["Arimo", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
