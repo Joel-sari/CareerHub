863edd1ea6db35e941222265018d56d25c0e5bff
Jacquelyn Phan <jacquelynphan108@gmail.com>
2025-10-26 13:33:20 -0400

    Fixing maps view frontend

--- CareerHub/settings.py
+++ CareerHub/settings.py
@@ -16,6 +16,8 @@ GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
+
 # Static files (global static folder at project root)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
